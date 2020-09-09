fea33632a82a94630b2618cdbf250a082691a07d
Nick Stenquist <whiteshadow148@gmail.com>
2020-09-09 09:38:00 -0600

    initial setup

---
+++ uora/uora/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for uora project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uora.settings')
+
+application = get_asgi_application()
